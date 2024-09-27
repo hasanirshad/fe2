3353ed7a0bb57e20af803e8add26a4a5ef2ad43b
Hasan <apple@Apples-MacBook-Air-2.local>
2024-09-27 07:29:33 -0500

    routing

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  async redirects() {
+    return [
+      {
+        source: '/',           // When users visit the root
+        destination: '/home',  // Redirect them to /Home
+        permanent: true,       // Set to true for a 301 permanent redirect
+      },
+    ];
+  },
 };
 
 export default nextConfig;
