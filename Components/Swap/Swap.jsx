03984c841de4cae80215b3b189ad72b0a31f47ce
Hasan <apple@Apples-MacBook-Air-2.local>
2024-09-30 06:16:03 -0500

    Updated: swap background image

--- Components/Swap/Swap.jsx
+++ Components/Swap/Swap.jsx
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import styles from "../Swap/Swap.module.css";
 import TokenInfo from "./SwapTokenInfo";
 import {  TokenSelector, SwapInput } from "../../Components/index.js";
+import Image from 'next/image';
+import swapBackground from '../../public/Assets/swap_back.png'
 
 
 const Swap = () => {
@@ -29,11 +31,16 @@ const Swap = () => {
   return (
     <main className={styles.swapContainer}>
       <section className={styles.swapSection}>
-        <img
+       {/*} <img
           src="https://cdn.builder.io/api/v1/image/assets/TEMP/8e18b20cae4d410bb8cbceecd2e47c4ade8aed704378de4ba8605025f83ba189?placeholderIfAbsent=true&apiKey=45ca834082bd4e999875d6aa728b9960"
           alt=""
           className={styles.backgroundImage}
-        />
+  />*/}
+  <Image
+          src={swapBackground}
+          alt=""
+          className={styles.backgroundImage}
+  />
         <img
         src="https://cdn.builder.io/api/v1/image/assets/TEMP/8925fa82a42490e128a2a2103ef2921b8fe1c3ee392ba2bb731a3b3a659c5b25?placeholderIfAbsent=true&apiKey=45ca834082bd4e999875d6aa728b9960"
         alt="Swap"
